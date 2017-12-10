d2aab7666d8fd60deb0dd4ab1037f9a105b1f486
Vic Luo <vicluo96@gmail.com>
2017-12-10 23:04:39 +0800

    test/CreateTables: delete old rows

--- src/test/kotlin/simpledb/CreateTables.kt
+++ src/test/kotlin/simpledb/CreateTables.kt
@@ -7,6 +7,8 @@ import simpledb.remote.SimpleDriver
 fun createTables() {
     val d = SimpleDriver()
     val conn = d.connect("jdbc:simpledb://localhost", java.util.Properties())
+    conn.createStatement().executeUpdate("delete from STUDENT")
+
     val stmt = conn.createStatement()
 
     var s = "create table STUDENT(SId int, SName varchar(10), MajorId int, GradYear int)"
@@ -23,6 +25,8 @@ fun createTables() {
     Assertions.assertEquals(0, stmt.executeUpdate(s))
     println("Table DEPT created.")
 
+    conn.createStatement().executeUpdate("delete from DEPT")
+
     s = "insert into DEPT(DId, DName) values "
     val deptvals = arrayOf("(10, 'compsci')", "(20, 'math')", "(30, 'drama')")
     for (i in deptvals.indices)
@@ -33,6 +37,7 @@ fun createTables() {
     Assertions.assertEquals(0, stmt.executeUpdate(s))
     println("Table COURSE created.")
 
+    conn.createStatement().executeUpdate("delete from COURSE")
     s = "insert into COURSE(CId, Title, DeptId) values "
     val coursevals = arrayOf("(12, 'db systems', 10)", "(22, 'compilers', 10)", "(32, 'calculus', 20)", "(42, 'algebra', 20)", "(52, 'acting', 30)", "(62, 'elocution', 30)")
     for (i in coursevals.indices)
@@ -43,6 +48,7 @@ fun createTables() {
     Assertions.assertEquals(0, stmt.executeUpdate(s))
     println("Table SECTION created.")
 
+    conn.createStatement().executeUpdate("delete from SECTION")
     s = "insert into SECTION(SectId, CourseId, Prof, YearOffered) values "
     val sectvals = arrayOf("(13, 12, 'turing', 2004)", "(23, 12, 'turing', 2005)", "(33, 32, 'newton', 2000)", "(43, 32, 'einstein', 2001)", "(53, 62, 'brando', 2001)")
     for (i in sectvals.indices)
@@ -53,6 +59,7 @@ fun createTables() {
     Assertions.assertEquals(0, stmt.executeUpdate(s))
     println("Table ENROLL created.")
 
+    conn.createStatement().executeUpdate("delete from ENROLL")
     s = "insert into ENROLL(EId, StudentId, SectionId, Grade) values "
     val enrollvals = arrayOf("(14, 1, 13, 'A')", "(24, 1, 43, 'C' )", "(34, 2, 43, 'B+')", "(44, 4, 33, 'B' )", "(54, 4, 53, 'A' )", "(64, 6, 53, 'A' )")
     for (i in enrollvals.indices)
